2aca04656ed148d72c8fe3616995af7d2866ec93
Ahmed Mostafa <ahmed.nasser.swe@gmail.com>
2017-01-25 03:08:59 +0100

    Merge branch 'master' of https://github.com/ahmednasserswe/Txty_ios
    Integrating to Github new repo

---
+++ Pods/Target Support Files/Pods-TxTy iosUITests/Pods-TxTy iosUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TxTy_iosUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TxTy_iosUITests
+@end
---
+++ Pods/Target Support Files/Pods-TxTy iosUITests/Pods-TxTy iosUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TxTy_iosUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TxTy_iosUITestsVersionString[];
+
